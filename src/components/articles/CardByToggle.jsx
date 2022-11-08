09605ac6efda6293f3e0f42afd95cc19c995a4c9
dhrgodms <dhrgodms123@gmail.com>
2022-11-09 03:08:42 +0900

    feature: FAQArea 추가- 색변경 필요

--- src/components/articles/CardByToggle.jsx
+++ src/components/articles/CardByToggle.jsx
@@ -1,48 +1,55 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled('div')`
+  border: 3px solid ${(props) => props.setting.borderColor};
+  border-radius: 30px;
+  margin: 20px 150px;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  height: auto;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background-color: ${(props) => !props.props && '#dfffb437'};
+  }
+`;
+
+const TextContainer = styled('div')`
+  padding-top: 30px;
+  line-height: 150%;
+  font-size: 20.3px;
+  padding-left: 10px;
+`;
+
 
 const CardByToggle = ({ title, text }) => {
+  const initialSetting = { borderColor: '#6D6D6D' };
+  const changedSetting = { borderColor: '#7E9E10' };
   const [isOpened, setIsOpened] = useState(false);
-  const initialSetting = {
-    borderColor: '#6D6D6D',
-    pos: '0',
-  };
-  const changedSetting = {
-    borderColor: '#7E9E10',
-    pos: '100%',
-  };
   const [styleSetting, setStyleSetting] = useState(initialSetting);
-
 
   // isOpen ? 닫 : 열
   return (
-    //TODO: 인라인css 리팩토링필요
-    <div
-      style={{
-        border: `2px solid ${styleSetting.borderColor}`,
-        borderRadius: '30px',
-        margin: '20px 150px',
-        padding: '30px',
-        display: 'flex',
-        flexDirection: 'column',
-        height: 'auto',
-        
-        transition: 'max-height 0.3s ease-out',
-      }}
-    >
+    //TODO: 인라인css 리팩토링필요, text 폰트 color 먹히도록 수정
+    <Container props={isOpened} setting={styleSetting}>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <span style={{ fontSize: '30px' }}>
-          {title}
-        </span>
-        <button onClick={() => { setIsOpened((prev) => !prev);
-        if(isOpened) setStyleSetting(changedSetting);
-        else{setStyleSetting(initialSetting);}}}>
-          {isOpened ? '닫기' : '열기'}
+        <span style={{ fontSize: '30px' }}>{title}</span>
+        <button
+          onClick={() => {
+            setIsOpened((prev) => !prev);
+            if (!isOpened) setStyleSetting(changedSetting);
+            else {
+              setStyleSetting(initialSetting);
+            }
+          }}
+        >
+          {isOpened ? '🔺' : '🔻'}
         </button>
       </div>
-      {isOpened && (
-        <div style={{ color: 'white', paddingTop: '30px',lineHeight: '150%', fontSize: '20.3px', paddingLeft: '10px'}}>{text}</div>
-      )}
-    </div>
+      {isOpened && <TextContainer>{text}</TextContainer>}
+    </Container>
   );
 };
 
