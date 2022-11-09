456b8a0a90c74fc90e06d6f07dd8dfa91073d7d9
dhrgodms <dhrgodms123@gmail.com>
2022-11-09 17:28:07 +0900

    chore: modify components in FAQArea.jsx

--- src/components/pages/main/FAQArea.jsx
+++ src/components/pages/main/FAQArea.jsx
@@ -5,23 +5,20 @@ import Anchor from '../../atoms/Anchor';
 import * as colors from "@styles/colors";
 
 const Container = styled('div')`
-  position: relative;
-  padding: 32px;
-  display: flex;
-  gap: 10px;
-  flex-direction: column;
   width: 100%;
-  max-width: 1350px;
+  justify-content: center;
+  position: relative;
 `;
+
 
 const TitleWrapper = styled('div')`
   grid-row: 1;
   display: flex;
   align-items: center;
   flex-direction: column;
+  margin-top: 52px;
   margin: 70px 100px;
-  white-space: nowrap;
-  
+  margin-bottom: 100px;
 `;
 
 const Title = styled('span')`
@@ -34,19 +31,20 @@ const B = styled(Title)`
 `;
 
 const ContentsContainer = styled('div')`
-  color: ${colors.primary40};
-`;
-
-const ContentsWrapper = styled('div')`
-  padding-top: 20px;
+  padding-top: 10px;
   padding-left: 20px;
 `;
 
-const TextWrapper = styled('div')`
-  color: ${colors.primary40};
+const Ul = styled('ul')`
+  padding-left: 20px;
 `;
 
-const Ul = styled('ul')`
+const Li = styled('li')`
+  color: #bac0a3;
+  padding-left: 20px;
+`;
+
+const Ol = styled('ol')`
   padding-left: 20px;
 `;
 
@@ -62,80 +60,61 @@ const FAQArea = forwardRef((props, ref) => {
       <CardByToggle initialOpen title="Q1. 예매 방법?">
         <ContentsContainer>
           tott 내 예매 방법은 두 가지 종류가 있습니다.
-          <ContentsWrapper>
-            <TextWrapper>
-              👉 일반 예매 : 현금 혹은 코인 결제 시, 로그인한 지갑으로 SBT
-              티켓이 발급됩니다.
-            </TextWrapper>
-            <TextWrapper>
-              👉 팬 사전 예매 : 주최 측에서 정한 콘서트 티켓(이미 사용한
-              SBT티켓) 보유 시, 사전 예매 좌석 구매 기회가 제공됩니다.
-            </TextWrapper>
-          </ContentsWrapper>
           <Ul>
             <br />
-            <li>
+            <Li>
               👉 일반 예매 : 현금 혹은 코인 결제 시, 로그인한 지갑으로 SBT
               티켓이 발급됩니다.
-            </li>
-            <li>
+            </Li>
+            <Li>
               👉 팬 사전 예매 : 주최 측에서 정한 콘서트 티켓(이미 사용한
               SBT티켓) 보유 시, 사전 예매 좌석 구매 기회가 제공됩니다.
-            </li>
+            </Li>
           </Ul>
         </ContentsContainer>
       </CardByToggle>
-      <CardByToggle
-        title="Q2. 티켓 이용 방법?"
-        text={
-          <div>
-            <ol>
-              <li>0. QR 발행은 모바일에서만 가능</li>
-              <li>1. 마이페이지-구매한 티켓-QR 발행하기 버튼 클릭</li>
-              <li>2. 휴대폰 본인 인증</li>
-              <li>
-                3. 구매한 티켓의 정보와 본인 인증 정보가 일치할 시, QR 발행
-              </li>
-              <li>
-                4. 발행된 QR은 당일 현장에서 리더기를 통해 매칭 후 입장 가능
-              </li>
-              <li>
-                5. 부정사용 방지를 위해 두 가지 정책을 따릅니다.
-                <Ul>
-                  <li>👉10초 마다 QR코드 갱신</li>
-                  <li>👉보안 정책상 캡처 불가</li>
-                </Ul>
-              </li>
-            </ol>
-          </div>
-        }
-      />
-      <CardByToggle
-        title="Q3. 팬 멤버쉽?"
-        text={
-          <div>
-            <ul>
-              <li>
-                👉 팬 래플 시스템을 구축하여 티켓이나 굿즈 등을 이벤트로
-                제공한다.
-              </li>
-              <li>
-                👉 카테고리별 티어 시스템(구매 횟수와 일정 구매 금액별)
-                <Ul>
-                  <li>🥈실버 : 할인 + 쿠폰 + 적립금</li>
-                  <li>
-                    🥇골드 : 할인 + 쿠폰 + 적립금, 관심 콘서트 등록 시 알림.
-                  </li>
-                  <li>
-                    💎플레티넘 : 할인 + 쿠폰 + 적립금, 취소 좌석 알림, 관심
-                    콘서트 등록 시 알림, (래플 참여권 1개)
-                  </li>
-                </Ul>
-              </li>
-            </ul>
-          </div>
-        }
-      />
+      <CardByToggle title="Q2. 티켓 이용 방법?">
+        <ContentsContainer>
+          <Ol>
+            <Li>0. QR 발행은 모바일에서만 가능</Li>
+            <Li>1. '마이페이지'-'구매한 티켓'-'QR 발행하기' 클릭</Li>
+            <Li>2. 휴대폰 본인 인증</Li>
+            <Li>3. 구매한 티켓의 정보와 본인 인증 정보가 일치할 시, QR 발행</Li>
+            <Li>
+              4. 발행된 QR은 당일 현장에서 리더기를 통해 매칭 후 입장 가능
+            </Li>
+            <Li>
+              5. 부정사용 방지를 위해 두 가지 정책을 따릅니다.
+              <Ul>
+                <Li>👉10초 마다 QR코드 갱신</Li>
+                <Li>👉보안 정책상 캡처 불가</Li>
+              </Ul>
+            </Li>
+          </Ol>
+        </ContentsContainer>
+      </CardByToggle>
+      <CardByToggle title="Q3. 팬 멤버쉽?">
+        <ContentsContainer>
+          <Ul>
+            <Li>
+              👉 팬 래플 시스템을 구축하여 티켓이나 굿즈 등을 이벤트로 제공한다.
+            </Li>
+            <Li>
+              👉 카테고리별 티어 시스템(구매 횟수와 일정 구매 금액별)
+              <Ul>
+                <Li>🥈실버 : 할인 + 쿠폰 + 적립금</Li>
+                <Li>
+                  🥇골드 : 할인 + 쿠폰 + 적립금, 관심 콘서트 등록 시 알림.
+                </Li>
+                <Li>
+                  💎플레티넘 : 할인 + 쿠폰 + 적립금, 취소 좌석 알림, 관심 콘서트
+                  등록 시 알림, (래플 참여권 1개)
+                </Li>
+              </Ul>
+            </Li>
+          </Ul>
+        </ContentsContainer>
+      </CardByToggle>
     </Container>
   );
 });
