b76c8ceae4662046b9c5353c1ebdfe0f5d9ee71b
dhrgodms <dhrgodms123@gmail.com>
2022-11-10 01:46:05 +0900

    feat: Add ToggleIcon to CardByToggle.jsx

--- src/components/articles/CardByToggle.jsx
+++ src/components/articles/CardByToggle.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Column } from '@components/atoms/wrapper.style';
+import { Column, Row } from '@components/atoms/wrapper.style';
 import * as colors from '@styles/colors';
 import { lg, sm } from '@styles/GlobalStyle';
+import { AiOutlineCaretUp } from 'react-icons/ai';
 
 
-//  [ing] 겹치는 것들 atom으로
+//  [O] 겹치는 것들 atom으로
 //  [O] props 없애주기
-//  [ ] div-span버튼 크게 하나로
+//  [O] div-span버튼 크게 하나로
 //  [O] border에서 A면 어떤거 아니면 기본
 //  [O] colors (1)가져다쓰고 (2)많이 쓰는거 등록
-//  [ ] inline 빼는게 좋다!(for 가독성)
+//  [O] inline 빼는게 좋다!(for 가독성)
 //  [ ] react-icons 이용해서 버튼 색 수정
 //  [O] 반응형 적용 (Roadmap, FAQ)
 
@@ -29,8 +30,7 @@ const ContentsContainer = styled(Column)`
   margin: 20px;
   padding: 28px;
   width: 80vw;
-  height: auto;
-  transition: background 0.1s ease-in-out;
+  transition: all 0.1s;
 
   &:hover {
     background-color: ${(props) => !props.isOpened && colors.bgFAQ};
@@ -40,12 +40,14 @@ const ContentsContainer = styled(Column)`
     gap: 23px;
     margin: 23px 44px;
     padding: 23px;
+    border-radius: 23px;
   `}
 
   ${sm`
     gap: 19px;
     margin: 19px 33px;
     padding: 19px;
+    border-radius: 19px;
   `}
 `;
 
@@ -56,6 +58,7 @@ const TextContainer = styled(Column)`
   align-items: center;
   justify-content: center;
   margin-top: 28px;
+  overflow: hidden;
 
   ${lg`
     font-size: 15px;
@@ -71,6 +74,9 @@ const TextContainer = styled(Column)`
 const TitleContainer = styled(TextContainer)`
   font-size: 28px;
   margin-top: 0px;
+  flex-direction: row;
+  transition: all 1s ease-in-out;
+
   ${lg`
     font-size: 23px;
   `}
@@ -80,6 +86,13 @@ const TitleContainer = styled(TextContainer)`
   `}
 `;
 
+const ToggleButton = styled(AiOutlineCaretUp)`
+  transition: all 0.3s ease-out;
+  transform: ${(props) =>
+    props.isOpened && 'rotate(180deg)'};
+  color: ${colors.primary80};
+  margin: 4px;
+`;
 
 
 
@@ -95,7 +108,8 @@ const CardByToggle = ({ title, children}) => {
     >
       <ContentsContainer isOpened={isOpened}>
         <TitleContainer>
-          <span>{title}</span>
+            <span>{title}</span>
+            <ToggleButton isOpened={isOpened} />
         </TitleContainer>
         {isOpened && <TextContainer>{children}</TextContainer>}
       </ContentsContainer>
