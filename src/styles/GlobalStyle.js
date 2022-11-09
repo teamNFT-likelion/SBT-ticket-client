157631d40bb7964fe51e635ad01473c6b9597a7c
dhrgodms <dhrgodms123@gmail.com>
2022-11-09 19:12:57 +0900

    chore: modify return components in CardByToggle

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -29,7 +29,6 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, menu, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, ruby, section, summary, time, mark, audio, video { 
     font-family: "Do Hyeon", sans-serif, -apple-system, BlinkMacSystemFont, system-ui, Roboto;
-    color: ${colors.textWhite};
     font-weight: 400;
   }
 
@@ -44,7 +43,8 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background-color: ${colors.bgBlack};
+    background-color: ${colors.bgBlack};    
+    color: ${colors.textWhite};
   }
 
   button {
