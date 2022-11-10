bb4a339080a201ccfe81165622837ce362c0244d
dhrgodms <dhrgodms123@gmail.com>
2022-11-10 16:50:59 +0900

    fix: Roadmap, FAQ 간격 수정

--- src/components/pages/main/RoadmapArea.jsx
+++ src/components/pages/main/RoadmapArea.jsx
@@ -5,33 +5,20 @@ import { lg, sm } from '@styles/GlobalStyle';
 import Anchor from '@components/atoms/Anchor';
 import { Column, Title } from '@components/atoms/wrapper.style';
 
-const Container = styled('div')`
-  width: 90%;
-  height: 90vh;
-  background: url(${bgRoadmap}) no-repeat center;
-  background-size: contain;
-  justify-content: center;
-
-  ${lg`
-    margin: 0px 44px;
-    height: 60vh;
-  `}
-
-  ${sm`
-    margin: 0px 33px;
-  `}
-`;
-
 
 const TitleColumn = styled(Column)`
   white-space: nowrap;
   margin-top: 15vh;
   position: relative;
   align-items: center;
-  margin: 5rem 0 2rem 0;
+  margin: 500px 0 6rem 0;
+
+  ${lg`
+    margin: 250px 0 2rem 0 ;
+  `}
 
   ${sm`
-    margin: 5rem 0 0 0;
+    margin: 150px 0 100px0;
   `}
 `;
 
@@ -43,7 +30,7 @@ const RoadmapArea = forwardRef((props, ref) => {
         <Anchor ref={ref} />
           <Title>ttot Roadmap</Title>
       </TitleColumn>
-      <Container />
+      <img src={bgRoadmap} alt="bgRoadmap" style={{ height: 'auto', width:'80vw'}} />
     </>
   );
 });
