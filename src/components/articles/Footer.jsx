8a27e3e54d6283a472fcf38a6a0adf89ef0d18c3
2yong-k <kkykdy4334@gmail.com>
2022-11-10 18:32:57 +0900

    :dizzy: Add Emoji - Footer

--- src/components/articles/Footer.jsx
+++ src/components/articles/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import * as colors from "@styles/colors";
-import ttotLogo from "@assets/img/logo_ttot.png";
+import * as colors from '@styles/colors';
+import ttotLogo from '@assets/img/logo_ttot.png';
+import { BsTwitter, BsYoutube, BsDiscord } from 'react-icons/bs';
 
 const Container = styled('div')`
   width: 100%;
@@ -12,14 +13,29 @@ const Container = styled('div')`
 `;
 const TtotLogoImage = styled('img')`
   height: 30px;
-  margin-bottom: 14px;
 `;
-const FooterTextWrapper = styled('div')`
+const EmojiWrapper = styled('div')`
   display: flex;
+  justify-content: center;
   align-items: center;
+  gap: 20px;
+`;
+const EmojiButton = styled('button')`
+  color: ${colors.primary80};
+  font-size: 30px;
+  cursor: pointer;
+`;
+const FooterHeadContainer = styled('div')`
+  display: flex;
+  justify-content: space-between;
   gap: 12px;
   margin-bottom: 10px;
-`
+`;
+const FooterTextContainer = styled('div')`
+  display: flex;
+  gap: 12px;
+  margin-bottom: 10px;
+`;
 const FooterText = styled.span`
   color: ${colors.textSecondary};
   font-size: 14px;
@@ -30,7 +46,7 @@ const FooterLinks = styled.div`
   align-items: center;
   width: 260px;
   margin-top: 26px;
-`
+`;
 const Divider = () => {
   return (
     <svg
@@ -43,20 +59,33 @@ const Divider = () => {
       <rect width={1} height={12} fill="#3F3F46" />
     </svg>
   );
-}
+};
 
 const Footer = () => {
   return (
     <Container>
-      <TtotLogoImage src={ttotLogo} />
-      <FooterTextWrapper>
+      <FooterHeadContainer>
+        <TtotLogoImage src={ttotLogo} />
+        <EmojiWrapper>
+          <EmojiButton>
+            <BsTwitter />
+          </EmojiButton>
+          <EmojiButton>
+            <BsYoutube />
+          </EmojiButton>
+          <EmojiButton>
+            <BsDiscord />
+          </EmojiButton>
+        </EmojiWrapper>
+      </FooterHeadContainer>
+      <FooterTextContainer>
         <FooterText>멋쟁이사자처럼 블록체인 스쿨 1기</FooterText>
         <FooterText>1팀</FooterText>
         <FooterText>TEAM NFT</FooterText>
-      </FooterTextWrapper>
-      <FooterTextWrapper>
+      </FooterTextContainer>
+      <FooterTextContainer>
         <FooterText>&copy; TTOT. 2022. All rights reserved</FooterText>
-      </FooterTextWrapper>
+      </FooterTextContainer>
       <FooterLinks>
         <FooterText>이용약관</FooterText>
         <Divider />
