250cb3150f371756755a4e6ba8895228bf4a39cc
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-11-16 01:15:52 +0900

    feat: 네이버 프로필 불러오기 테스트

--- src/components/pages/APaymentPage.jsx
+++ src/components/pages/APaymentPage.jsx
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '@articles/Header';
-import { TempWrapper } from '../atoms/wrapper.style';
+import axios from 'axios';
+import { TempWrapper } from '@atoms/wrapper.style';
+import { naverOauthUrl } from '@constants/urlConst';
+import { getCookie } from '@utils/cookie';
 
 const APaymentPage = () => {
+  const oauthData = getCookie('oauthData');
+  const onClick = async () => {
+    const res = await axios.get('http://localhost:5000/test');
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    console.log(oauthData);
+  }, [oauthData]);
+
   return (
     <TempWrapper>
       <div>결제페이지~</div>
       <Header />
       결제 1단계...
+      <button
+        style={{
+          border: '2px solid green',
+          color: 'green',
+          backgroundColor: 'white',
+          width: '300px',
+          height: '100px',
+          fontSize: '30px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        onClick={onClick}
+      >
+        api테스트
+      </button>
+      <a
+        style={{
+          border: '2px solid green',
+          color: 'green',
+          backgroundColor: 'white',
+          width: '300px',
+          height: '100px',
+          fontSize: '30px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        href={naverOauthUrl}
+      >
+        네이버 테스트
+      </a>
     </TempWrapper>
   );
 };
