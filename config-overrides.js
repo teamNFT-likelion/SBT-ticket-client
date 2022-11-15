e5b18b560e04dc5f3fc558ecdac61faa3452cb71
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-11-16 01:12:04 +0900

    chore: react-cookie 설치및 utils 폴더 경로설정

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,17 @@
-const { override, addWebpackAlias, addBabelPlugins } = require("customize-cra");
-const path = require("path");
+const { override, addWebpackAlias, addBabelPlugins } = require('customize-cra');
+const path = require('path');
 
 module.exports = override(
-	addWebpackAlias({
-		"@components": path.resolve(__dirname, "src", "components"),
-		"@assets": path.resolve(__dirname, "src", "assets"),
-		"@styles": path.resolve(__dirname, "src", "styles"),
-		"@util": path.resolve(__dirname, "src", "util"),
-		"@hooks": path.resolve(__dirname, "src", "hooks"),
-		"@constants": path.resolve(__dirname, "src", "constants"),
-		"@pages": path.resolve(__dirname, "src/components", "pages"),
-		"@atoms": path.resolve(__dirname, "src/components", "atoms"),
-		"@articles": path.resolve(__dirname, "src/components", "articles"),
-	}),
-	...addBabelPlugins(["babel-plugin-styled-components"])
+  addWebpackAlias({
+    '@components': path.resolve(__dirname, 'src', 'components'),
+    '@assets': path.resolve(__dirname, 'src', 'assets'),
+    '@styles': path.resolve(__dirname, 'src', 'styles'),
+    '@utils': path.resolve(__dirname, 'src', 'utils'),
+    '@hooks': path.resolve(__dirname, 'src', 'hooks'),
+    '@constants': path.resolve(__dirname, 'src', 'constants'),
+    '@pages': path.resolve(__dirname, 'src/components', 'pages'),
+    '@atoms': path.resolve(__dirname, 'src/components', 'atoms'),
+    '@articles': path.resolve(__dirname, 'src/components', 'articles'),
+  }),
+  ...addBabelPlugins(['babel-plugin-styled-components']),
 );
