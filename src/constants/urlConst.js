19420985c3c1aa298999bf694c4b00c6e3cef9d5
dhrgodms <dhrgodms123@gmail.com>
2022-11-22 17:43:48 +0900

    fix : kakao log in api 관련 urlConst.js 수정

--- src/constants/urlConst.js
+++ src/constants/urlConst.js
@@ -5,7 +5,9 @@ const naverRedirectUri = window.origin.includes('localhost')
   : 'https://ttot.netlify.app/payment';
 export const naverOauthUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverClientId}&state=${naverState}&redirect_uri=${naverRedirectUri}`;
 
+const kakaoState = 'kakao';
 const kakaoClientId = process.env.REACT_APP_KAKAO_CLIENT_ID;
-const kakaoRedirectUri = 'http://localhost:5000/kakao/callback'; //TODO: 환경고려 코드수정필요
-const kakaoState = window.origin.includes('localhost') ? 'local' : 'dev'; //TODO: 환경고려 코드수정필요
+const kakaoRedirectUri = window.origin.includes('localhost')
+  ? 'http://localhost:3000/payment'
+  : 'https://ttot.netlify.app/payment';
 export const kakaoOauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${kakaoRedirectUri}&response_type=code&state=${kakaoState}`;
