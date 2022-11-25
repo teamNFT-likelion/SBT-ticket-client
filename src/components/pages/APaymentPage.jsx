f90f45e9dd0d43b49280fdab3ec6fda7c7d4e658
dhrgodms <dhrgodms123@gmail.com>
2022-11-26 01:22:19 +0900

    PORT 5000 -> 443

--- src/components/pages/APaymentPage.jsx
+++ src/components/pages/APaymentPage.jsx
@@ -17,7 +17,7 @@ const APaymentPage = () => {
     // 인가코드 서버에 전달 및 프로필데이터 응답처리
     const getOauthData = (_state, _code) => {
       axios
-        .get(`https://13.114.237.105:5000/${_state}/callback`, {
+        .get(`https://13.114.237.105:443/${_state}/callback`, {
           params: {
             code: _code,
           },
