a1f1794ad3beb4cfedf24ec0f74b9419a97a15e8
dhrgodms <dhrgodms123@gmail.com>
2022-11-25 22:41:25 +0900

    fix : add :80 (PORT NUMBER)

--- src/constants/urlConst.js
+++ src/constants/urlConst.js
@@ -5,12 +5,12 @@ const naverClientId = process.env.REACT_APP_NAVER_CLIENT_ID;
 //   : 'https://ttot.netlify.app/payment';
 const naverRedirectUri = window.origin.includes('localhost')
   ? 'http://localhost:3000/payment'
-  : 'https://13.114.237.105/payment';
+  : 'https://13.114.237.105:80/payment';
 export const naverOauthUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverClientId}&state=${naverState}&redirect_uri=${naverRedirectUri}`;
 
 const kakaoState = 'kakao';
 const kakaoClientId = process.env.REACT_APP_KAKAO_CLIENT_ID;
 const kakaoRedirectUri = window.origin.includes('localhost')
   ? 'http://localhost:3000/payment'
-  : 'https://13.114.237.105/payment';
+  : 'https://13.114.237.105:80/payment';
 export const kakaoOauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${kakaoRedirectUri}&response_type=code&state=${kakaoState}`;
