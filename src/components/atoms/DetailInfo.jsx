c3f2e330dd4b1de8fa83726b2968494a854a1fcb
dhrgodms <dhrgodms123@gmail.com>
2022-11-29 01:24:44 +0900

    feat : 공연 Detail 정보 컴포넌트 (atom) 폴더에 생성

---
+++ src/components/atoms/DetailInfo.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Column, Row } from '@components/atoms/wrapper.style';
+
+const ContentsInfoBody = styled(Row)`
+  // HEADER 높이 5rem + 여분 9rem
+  color: white;
+  display: flex;
+  justify-content: center;
+  margin: 5rem;
+`;
+
+const ContentsInfo = styled(Column)`
+  width: 273px;
+  height: 345px;
+  border: 3px solid white;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DetailInfo = () => {
+  return (
+    <ContentsInfoBody>
+      <ContentsInfo>Info</ContentsInfo>
+      <ContentsInfo>Poster</ContentsInfo>
+    </ContentsInfoBody>
+  );
+};
+
+export default DetailInfo;
