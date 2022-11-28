1022e32eb8d4743907e36a66044c759a6ccc18a1
dhrgodms <dhrgodms123@gmail.com>
2022-11-29 01:23:57 +0900

    fix : 주석 삭제 in CardByToggle

--- src/components/articles/CardByToggle.jsx
+++ src/components/articles/CardByToggle.jsx
@@ -6,17 +6,6 @@ import { lg, sm } from '@styles/GlobalStyle';
 import { AiOutlineCaretUp } from 'react-icons/ai';
 
 
-//  [O] 겹치는 것들 atom으로
-//  [O] props 없애주기
-//  [O] div-span버튼 크게 하나로
-//  [O] border에서 A면 어떤거 아니면 기본
-//  [O] colors (1)가져다쓰고 (2)많이 쓰는거 등록
-//  [O] inline 빼는게 좋다!(for 가독성)
-//  [O] react-icons 이용해서 버튼 색 수정
-//  [O] 반응형 적용 (Roadmap, FAQ)
-//  [ ] Answer 열리는거 transtition 적용해주고싶다
-
-
 const CardContainer = styled(Column)`
   border: 3px solid
     ${(props) => (props.isOpened ? colors.primary40 : colors.bgFAQ)};
