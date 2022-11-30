e0505d53ad51891586a2339a5d1be93f829ff073
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-11-30 17:15:47 +0900

    feat: 헤더 로고 라우팅경로 landing-page > tlp-page 수정

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ScrollToTop from '@articles/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
   <>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
     <ToastContainer theme="dark" />
   </>,
-  // </React.StrictMode>,
 );
