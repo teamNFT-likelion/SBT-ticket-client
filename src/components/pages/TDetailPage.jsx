b84e2508e5ae83991be0d207b13c80cf033d72f5
dhrgodms <dhrgodms123@gmail.com>
2022-12-06 23:58:24 +0900

    rebase to dy/ttot

--- src/components/pages/TDetailPage.jsx
+++ src/components/pages/TDetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import * as colors from '@styles/colors';
 // import styled from 'styled-components';
 import Layout from '@articles/Layout';
@@ -13,19 +13,42 @@ import { ContentsInfoBody, SelectInfo } from '@styles/ticketDetailStyle';
 import TDPCalendar from '@components/atoms/Calendar';
 import PartInfoContainer from '@components/articles/PartInfoContainer';
 import RemainSeats from '@components/atoms/RemainSeats';
+<<<<<<< HEAD
 import Page404 from './Page404';
+=======
+import NullData from './NullData';
+import CheckWallet from '@components/wallets/components/CheckWallet';
+>>>>>>> c1cc5df (지갑 연결되어있는지 확인)
 
 const TDetailPage = () => {
   const location = useLocation();
   const [partState, setPartState] = useState(0);
   const parsed = parse(location.search);
+
+  const [connect, setConnect] = useState('uninstalled');
+
   const dataId = parsed.id;
 
   const data = mainItems.filter((item) => item.id === dataId)[0] || false;
 
+  const checkwallet = CheckWallet();
+
   let pageComponent;
 
-  if (data) {
+  useEffect(() => {
+    if (
+      typeof window.ethereum === 'undefined' &&
+      typeof window.klaytn === 'undefined'
+    ) {
+      setConnect('none');
+    } else if (window.ethereum) {
+      console.log(window.ethereum._state._isUnlocked);
+    } else {
+      console.log(window.klaytn._state._isUnlocked);
+    }
+  }, [connect]);
+
+  if (data && (connect === 'klaytn' || connect === 'ethereum')) {
     pageComponent = (
       <Layout>
         <CategoryNav />
@@ -47,6 +70,12 @@ const TDetailPage = () => {
         <ContentsInfoBody>
           <img src={data.detailInfoImg} alt="detailInfo" width={'800px'} />
         </ContentsInfoBody>
+      </Layout>
+    );
+  } else if (data) {
+    pageComponent = (
+      <Layout>
+        <CategoryNav />
       </Layout>
     );
   } else {
