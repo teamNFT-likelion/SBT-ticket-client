3838d50caf18ff216d562587d7c2d63370b56860
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-12-17 10:30:55 +0900

    feat: app4 step 수정

--- src/components/pages/payment/App4Done.jsx
+++ src/components/pages/payment/App4Done.jsx
@@ -1,13 +1,19 @@
 import LinkButton from '@atoms/LinkButton';
-import { PageTitle, SubTitle, TempBox } from '@styles/ApaymentStyles';
+import { PageTitle, SubTitle } from '@styles/ApaymentStyles';
+import { StepBox, LeftBox, RightBox } from './App1Start';
 
-export const App4Done = () => {
+export const App4Done = ({ data }) => {
+  //TODO: 공연data + 선택값들 recoil selector로 받아와서 보여주는 화면 기획필요
   return (
-    <>
-      <PageTitle>결제가 완료되었습니다.</PageTitle>
-      <SubTitle>| 결제 정보 |</SubTitle>
-      <TempBox>결제된 티켓의 정보</TempBox>
-      <LinkButton to="/account" name="티켓 확인하러가기" />
-    </>
+    <StepBox>
+      <LeftBox>
+        <PageTitle>결제가 완료되었습니다.</PageTitle>
+        <SubTitle>| 결제 정보 |</SubTitle>
+      </LeftBox>
+      <RightBox>
+        ...???
+        <LinkButton to="/account" name="티켓 확인하러가기" />
+      </RightBox>
+    </StepBox>
   );
 };
